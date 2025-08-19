524e58eda7857b26d9c75c1011ecf6eb2fa5166f
anku1506 <kumaranku946@gmail.com>
2025-08-19 22:37:04 +0530

    Create Basic setup

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CreateTodo from './CreateTodo';
+
+const Card = () => {
+  return (
+      <>
+          <h3>From Card calling createtodo</h3>
+          <CreateTodo/>
+
+      </>
+          
+  
+  )
+}
+
+export default Card;
---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateTodo = () => {
+  return (
+      <div>
+          <h1>CreateTodo</h1>
+    </div>
+  )
+}
+
+export default CreateTodo;
